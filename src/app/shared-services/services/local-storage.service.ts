9e2f05e1ce409ed067c006e9cd265ea642eb083c
Subarna Bijaya Khadka <khadka.subarnabijaya@gmail.com>
2021-10-21 01:34:21 -0400

    configured local storage service

--- src/app/shared-services/services/local-storage.service.ts
+++ src/app/shared-services/services/local-storage.service.ts
@@ -6,4 +6,38 @@ import { Injectable } from '@angular/core';
 export class LocalStorageService {
 
   constructor() { }
+
+  // Setting token
+  setToken(token): boolean {
+    localStorage.setItem('token', token);
+    return true;
+  }
+
+  // Getting token
+  getRefreshToken(): string {
+    return localStorage.getItem('RefreshToken');
+  }
+
+  // Setting token
+  setRefreshToken(token): boolean {
+    localStorage.setItem('RefreshToken', token);
+    return true;
+  }
+
+  // Getting token
+  getToken(): string {
+    return localStorage.getItem('token');
+  }
+
+  // check if user is logged in or not
+  isLoggedIn(): boolean {
+    const token = localStorage.getItem('token');
+    return !!token;
+  }
+
+  // logs out user
+  logout(): boolean {
+    localStorage.removeItem('token');
+    return true;
+  }
 }
