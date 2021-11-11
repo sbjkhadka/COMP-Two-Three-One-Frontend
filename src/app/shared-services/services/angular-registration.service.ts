c8ff8dd78bd6379a06252edf36f961dc9152a251
Subarna Bijaya Khadka <khadka.subarnabijaya@gmail.com>
2021-11-11 04:04:30 -0500

    Created and implemented angular registration service

--- src/app/shared-services/services/angular-registration.service.ts
+++ src/app/shared-services/services/angular-registration.service.ts
@@ -3,6 +3,7 @@ import {HttpClient} from '@angular/common/http';
 import {Observable, throwError} from 'rxjs';
 import {environmentAngular} from '../../../environments/environment';
 import {catchError} from 'rxjs/operators';
+import {User} from '../../shared-models/user.model';
 
 @Injectable({
   providedIn: 'root'
@@ -11,8 +12,8 @@ export class AngularRegistrationService {
 
   constructor(private http: HttpClient) { }
 
-  signIn(credentials: any): Observable<any> {
-    return this.http.post<any>(`${environmentAngular.base_url}register`, credentials).pipe(catchError(error => {
+  register(user: User): Observable<any> {
+    return this.http.post<any>(`${environmentAngular.base_url}register`, user).pipe(catchError(error => {
       return throwError(error);
     }));
   }
