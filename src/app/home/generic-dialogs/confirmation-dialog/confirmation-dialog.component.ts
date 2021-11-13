3fd400c37a46b0e5a1ff0d7464d4d8d6c6689379
Subarna Bijaya Khadka <khadka.subarnabijaya@gmail.com>
2021-11-13 09:12:13 -0500

    confirmation dialog theming

--- src/app/home/generic-dialogs/confirmation-dialog/confirmation-dialog.component.ts
+++ src/app/home/generic-dialogs/confirmation-dialog/confirmation-dialog.component.ts
@@ -1,5 +1,6 @@
 import {Component, Inject, OnInit} from '@angular/core';
 import {MAT_DIALOG_DATA, MatDialogRef} from '@angular/material/dialog';
+import {ThemeService} from '../../../shared-services/theme.service';
 
 @Component({
   selector: 'app-confirmation-dialog',
@@ -8,17 +9,22 @@ import {MAT_DIALOG_DATA, MatDialogRef} from '@angular/material/dialog';
 })
 export class ConfirmationDialogComponent implements OnInit {
 
+  theme: string;
   itemName;
   itemType;
   constructor(
     private dialogRef: MatDialogRef<ConfirmationDialogComponent>,
-    @Inject(MAT_DIALOG_DATA) data) {
+    @Inject(MAT_DIALOG_DATA) data,
+    private themeService: ThemeService) {
     dialogRef.disableClose = true;
     this.itemName = data.itemName;
     this.itemType = data.itemType;
   }
 
   ngOnInit(): void {
+    this.themeService.theme.subscribe(value => {
+      this.theme = value;
+    });
   }
 
   close(decision: boolean): void {
