890549d7386ea09077045cbbe2656d3098eed519
Subarna Bijaya Khadka <khadka.subarnabijaya@gmail.com>
2021-11-13 02:56:59 -0500

    app component theming

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,6 +2,7 @@ import {Component, OnInit} from '@angular/core';
 import {FirebaseService} from './shared-services/services/firebase.service';
 import {AngularLoginService} from './shared-services/services/angular-login.service';
 import {LocalStorageService} from './shared-services/services/local-storage.service';
+import {ThemeService} from './shared-services/theme.service';
 
 @Component({
   selector: 'app-root',
@@ -11,14 +12,20 @@ import {LocalStorageService} from './shared-services/services/local-storage.serv
 export class AppComponent implements OnInit{
   title = 'COMP-Two-Three-One-Frontend';
   loggedInUser;
+  theme: string;
+
   constructor(public firebaseService: FirebaseService,
               private angularLoginService: AngularLoginService,
-              private localStorageService: LocalStorageService) {
+              private localStorageService: LocalStorageService,
+              private themeService: ThemeService) {
 
   }
 
   ngOnInit(): void {
     this.loggedInUser =  JSON.parse(localStorage.getItem('logged_in_user'));
+    this.themeService.theme.subscribe(value => {
+      this.theme = value;
+    });
   }
 
   logout(): any {
@@ -26,5 +33,9 @@ export class AppComponent implements OnInit{
     this.localStorageService.removeToken();
     this.localStorageService.removeRefreshToken();
     this.angularLoginService.logout();
+  }
+
+  switchTheme(): void {
+    this.themeService.switchTheme();
   }
 }
