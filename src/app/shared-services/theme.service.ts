9c546db73f2b9537ed7e84b60b31f6dd5ecb7b41
Subarna Bijaya Khadka <khadka.subarnabijaya@gmail.com>
2021-11-13 04:52:38 -0500

    theme service upgraded

--- src/app/shared-services/theme.service.ts
+++ src/app/shared-services/theme.service.ts
@@ -1,22 +1,35 @@
-import { Injectable } from '@angular/core';
+import {Injectable, OnInit} from '@angular/core';
 import {BehaviorSubject} from 'rxjs';
+import {LocalStorageService} from './services/local-storage.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ThemeService {
 
-  constructor() { }
+  constructor(private localStorageService: LocalStorageService) {
+    const theme = this.localStorageService.getItem('theme');
+    if (theme) {
+      this.theme.next(theme);
+    } else {
+      this.theme.next('color');
+      this.localStorageService.setItem('theme', 'color');
+    }
+    this.localStorageService.getItem('theme');
+  }
 
   theme = new BehaviorSubject<string>('color');
 
   switchTheme(): void {
     if (this.theme.getValue() === 'color') {
       this.theme.next('dark');
+      this.localStorageService.setItem('theme', 'dark');
     } else if (this.theme.getValue() === 'dark') {
       this.theme.next('color');
+      this.localStorageService.setItem('theme', 'color');
     } else {
       this.theme.next('color');
+      this.localStorageService.setItem('theme', 'color');
     }
   }
 }
