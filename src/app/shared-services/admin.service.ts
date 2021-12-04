7f58ef2e8fbd1006a05e29acb0aedeafd5d4c5df
Subarna Bijaya Khadka <khadka.subarnabijaya@gmail.com>
2021-12-04 02:43:03 -0500

    admin service create support feedback added

--- src/app/shared-services/admin.service.ts
+++ src/app/shared-services/admin.service.ts
@@ -1,9 +1,20 @@
 import { Injectable } from '@angular/core';
+import {HttpClient} from '@angular/common/http';
+import {Observable, throwError} from 'rxjs';
+import {ContactUsModel} from '../shared-models/contact-us.model';
+import {environmentAngular} from '../../environments/environment';
+import {catchError} from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AdminService {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  sendFeedbackOrRequest(contactUsModel: ContactUsModel): Observable<any> {
+    return this.http.post<any>(`${environmentAngular.base_url}feedback`, contactUsModel).pipe(catchError(error => {
+      return throwError(error);
+    }));
+  }
 }
