36f98394a0152f19c8ff8c51bee1ad4124ef3698
Subarna Bijaya Khadka <khadka.subarnabijaya@gmail.com>
2021-12-04 07:05:06 -0500

    Support and feedback details component rocking!!

--- src/app/support-feedback/support-feedback.component.ts
+++ src/app/support-feedback/support-feedback.component.ts
@@ -1,4 +1,11 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit, ViewChild} from '@angular/core';
+import {AdminService} from '../shared-services/admin.service';
+import {ThemeService} from '../shared-services/theme.service';
+import {MatDialog} from '@angular/material/dialog';
+import {MatPaginator} from '@angular/material/paginator';
+import {BehaviorSubject} from 'rxjs';
+import {MatTableDataSource} from '@angular/material/table';
+import {FormControl} from '@angular/forms';
 
 @Component({
   selector: 'app-support-feedback',
@@ -6,10 +13,58 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./support-feedback.component.css']
 })
 export class SupportFeedbackComponent implements OnInit {
+  control = new FormControl('all');
+  heading = 'Supports and Feedbacks';
+  @ViewChild(MatPaginator) paginator: MatPaginator;
+  theme: string;
+  feedbacks = [];
+  feedbackTemp = [];
+  displayedColumns: string[] = ['id', 'type', 'email', 'message', 'action'];
+  dataSource = new BehaviorSubject<MatTableDataSource<any>>(new MatTableDataSource<any>([]));
 
-  constructor() { }
+  constructor(private adminService: AdminService,
+              private themeService: ThemeService) { }
 
   ngOnInit(): void {
+    this.themeService.theme.subscribe(value => {
+      this.theme = value;
+    });
+    this.getAllSupportsAndFeedbacks();
+    this.control.valueChanges.subscribe(value => {
+    this.feedbackTemp = this.feedbacks;
+    switch (value) {
+        case 'all': {
+          this.feedbackTemp = this.feedbacks;
+          this.heading = 'Supports and Feedbacks';
+          break;
+        } default: {
+          this.feedbackTemp = this.feedbacks.filter(feedback => feedback.type === value);
+          this.heading = value;
+          break;
+        }
+      }
+    this.dataSource.next(new MatTableDataSource<any>(this.feedbackTemp));
+    this.dataSource.value.paginator = this.paginator;
+    });
+  }
+
+  getAllSupportsAndFeedbacks(): void {
+    this.adminService.getAllSupportsAndFeedbacks().subscribe(feedbacks => {
+      console.log('feedbacks', feedbacks);
+      this.feedbacks = feedbacks.feedbacks;
+      this.feedbackTemp = feedbacks.feedbacks;
+      this.dataSource = new BehaviorSubject<MatTableDataSource<any>>(new MatTableDataSource<any>(feedbacks.feedbacks));
+      this.dataSource.value.paginator = this.paginator;
+    });
+  }
+
+  navigateToDetailsPage(element: any): void {
+    window.location.href = `/supportAndFeedback/${element._id}`;
+  }
+
+  applyFilter(event: Event): void {
+    const filterValue = (event.target as HTMLInputElement).value;
+    this.dataSource.value.filter = filterValue.trim().toLowerCase();
   }
 
 }
