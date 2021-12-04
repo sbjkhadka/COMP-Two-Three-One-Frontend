e78b6331ef6760a54efda5c26a1789da81217e12
Subarna Bijaya Khadka <khadka.subarnabijaya@gmail.com>
2021-12-04 07:04:39 -0500

    support and feedback detail component created

---
+++ src/app/support-feedback/support-feedback-details/support-feedback-details.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-support-feedback-details',
+  templateUrl: './support-feedback-details.component.html',
+  styleUrls: ['./support-feedback-details.component.css']
+})
+export class SupportFeedbackDetailsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
