bdc28d0e78810e6d03fe4ace9803b21136342db4
Subarna Bijaya Khadka <khadka.subarnabijaya@gmail.com>
2021-12-05 04:45:18 -0500

    updated contact us model

--- src/app/shared-models/contact-us.model.ts
+++ src/app/shared-models/contact-us.model.ts
@@ -1,6 +1,10 @@
+import {FeedbackDetailsModel} from './feedback-details.model';
+
 export class ContactUsModel {
   type: string;
   message: string;
   userEmail?: string;
   user?: string;
+  status?: string;
+  feedbackDetails?: FeedbackDetailsModel[];
 }
