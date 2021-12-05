f12843390f517b90ee8857affbf7c10ec1467866
Subarna Bijaya Khadka <khadka.subarnabijaya@gmail.com>
2021-12-05 07:06:02 -0500

    Added spinner in login

--- src/app/angular-login/angular-login.component.ts
+++ src/app/angular-login/angular-login.component.ts
@@ -16,6 +16,7 @@ import {SessionStorageService} from '../shared-services/session-storage.service'
 })
 export class AngularLoginComponent implements OnInit, OnDestroy {
 
+  spinnerShowing = false;
   theme: string;
   constructor(private angularLoginService: AngularLoginService,
               private localStorageService: LocalStorageService,
@@ -59,11 +60,13 @@ export class AngularLoginComponent implements OnInit, OnDestroy {
     );
   }
   onSignIn(): void {
+    this.spinnerShowing = true;
     this.credentials.email =  this.loginForm.value.email;
     this.credentials.password = this.loginForm.value.password;
 
     if (this.loginForm.value.email && this.loginForm.value.password) {
       this.angularLoginService.signIn(this.credentials).subscribe(value => {
+        this.spinnerShowing = false;
         if (value && value.accessToken) {
           this.sessionStorageService.setItem('logged_in_user', JSON.stringify(value));
           this.sessionStorageService.setToken(value.accessToken);
@@ -73,6 +76,8 @@ export class AngularLoginComponent implements OnInit, OnDestroy {
           this.sessionStorageService.setRefreshToken(value.refreshToken);
         }
         window.location.href = '/home';
+      }, error => {
+        this.spinnerShowing = false;
       });
     }
   }
